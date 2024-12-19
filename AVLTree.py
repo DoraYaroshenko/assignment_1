6456efc3991525fb63c8fa9935cedbf8f89af26c
dora <yar-dora@yandex.ru>
2024-12-19 15:23:28 +0200

    version with a lot of bugs

--- AVLTree.py
+++ AVLTree.py
@@ -1,176 +1,287 @@
-#id1:
-#name1:
-#username1:
-#id2:
-#name2:
-#username2:
-
+# id1:
+# name1:
+# username1:
+# id2:
+# name2:
+# username2:
 
 """A class represnting a node in an AVL tree"""
 
+
 class AVLNode(object):
-	"""Constructor, you are allowed to add more fields. 
-	
-	@type key: int
-	@param key: key of your node
-	@type value: string
-	@param value: data of your node
-	"""
-	def __init__(self, key, value):
-		self.key = key
-		self.value = value
-		self.left = None
-		self.right = None
-		self.parent = None
-		self.height = -1
-		
+    """Constructor, you are allowed to add more fields.
 
-	"""returns whether self is not a virtual node 
+    @type key: int
+    @param key: key of your node
+    @type value: string
+    @param value: data of your node
+    """
 
-	@rtype: bool
-	@returns: False if self is a virtual node, True otherwise.
-	"""
-	def is_real_node(self):
-		return False
+    def __init__(self, key, value, height=-1, parent = None):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = parent
+        self.height = height
+
+    """returns whether self is not a virtual node 
+
+    @rtype: bool
+    @returns: False if self is a virtual node, True otherwise.
+    """
+
+    def is_real_node(self):
+        if self.key is None or self is None:
+            return False
+        return True
+
+    def is_leaf(self):
+        return not self.left.is_real_node() and not self.right.is_real_node()
 
 
 """
 A class implementing an AVL tree.
 """
 
+
 class AVLTree(object):
+    """
+    Constructor, you are allowed to add more fields.
+    """
 
-	"""
-	Constructor, you are allowed to add more fields.
-	"""
-	def __init__(self):
-		self.root = None
+    def __init__(self, root=None):
+        self.root: AVLNode = root
 
-
-	"""searches for a node in the dictionary corresponding to the key (starting at the root)
+    """searches for a node in the dictionary corresponding to the key (starting at the root)
         
-	@type key: int
-	@param key: a key to be searched
-	@rtype: (AVLNode,int)
-	@returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
-	and e is the number of edges on the path between the starting node and ending node+1.
-	"""
-	def search(self, key):
-		return None, -1
+    @type key: int
+    @param key: a key to be searched
+    @rtype: (AVLNode,int)
+    @returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
+    and e is the number of edges on the path between the starting node and ending node+1.
+    """
 
+    def search(self, key):
+        return None, -1
 
-	"""searches for a node in the dictionary corresponding to the key, starting at the max
+    """searches for a node in the dictionary corresponding to the key, starting at the max
         
-	@type key: int
-	@param key: a key to be searched
-	@rtype: (AVLNode,int)
-	@returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
-	and e is the number of edges on the path between the starting node and ending node+1.
-	"""
-	def finger_search(self, key):
-		return None, -1
+    @type key: int
+    @param key: a key to be searched
+    @rtype: (AVLNode,int)
+    @returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
+    and e is the number of edges on the path between the starting node and ending node+1.
+    """
 
+    def finger_search(self, key):
+        return None, -1
 
-	"""inserts a new node into the dictionary with corresponding key and value (starting at the root)
+    """inserts a new node into the dictionary with corresponding key and value (starting at the root)
 
-	@type key: int
-	@pre: key currently does not appear in the dictionary
-	@param key: key of item that is to be inserted to self
-	@type val: string
-	@param val: the value of the item
-	@rtype: (AVLNode,int,int)
-	@returns: a 3-tuple (x,e,h) where x is the new node,
-	e is the number of edges on the path between the starting node and new node before rebalancing,
-	and h is the number of PROMOTE cases during the AVL rebalancing
-	"""
-	def insert(self, key, val):
-		return None, -1, -1
+    @type key: int
+    @pre: key currently does not appear in the dictionary
+    @param key: key of item that is to be inserted to self
+    @type val: string
+    @param val: the value of the item
+    @rtype: (AVLNode,int,int)
+    @returns: a 3-tuple (x,e,h) where x is the new node,
+    e is the number of edges on the path between the starting node and new node before rebalancing,
+    and h is the number of PROMOTE cases during the AVL rebalancing
+    """
 
+    def rotate_right(self):
+        root = self.root
+        new_root = self.root.left
+        new_root.parent = root.parent
+        root.left = new_root.right
+        root.left.parent = root
+        new_root.right = root
+        new_root.right.parent = new_root
+        self.root = new_root
+        return new_root
 
-	"""inserts a new node into the dictionary with corresponding key and value, starting at the max
+    def rotate_left(self):
+        root = self.root
+        new_root = root.right
+        new_root.parent = root.parent
+        root.right = new_root.left
+        root.right.parent = root
+        new_root.left = root
+        new_root.left.parent = new_root
+        self.root = new_root
+        return new_root
 
-	@type key: int
-	@pre: key currently does not appear in the dictionary
-	@param key: key of item that is to be inserted to self
-	@type val: string
-	@param val: the value of the item
-	@rtype: (AVLNode,int,int)
-	@returns: a 3-tuple (x,e,h) where x is the new node,
-	e is the number of edges on the path between the starting node and new node before rebalancing,
-	and h is the number of PROMOTE cases during the AVL rebalancing
-	"""
-	def finger_insert(self, key, val):
-		return None, -1, -1
+    def double_rotation(self):
+        root = self.root
+        root.left = AVLTree(root.left).rotate_left()
+        return self.rotate_right()
 
+    def rebalance(self, node):
+        promotions = 0
+        parent = node.parent
+        left_diff = parent.height - parent.left.height
+        right_diff = parent.height - parent.right.height
+        if left_diff == 1 and right_diff == 1:
+            return 0
+        elif (left_diff == 0 and right_diff == 1) or (left_diff == 1 and right_diff == 0):
+            parent.height += 1
+            promotions += 1 + self.rebalance(parent)
+        elif (left_diff == 0 and right_diff == 2) or (left_diff == 2 and right_diff == 0):
+            node_children_left_diff = node.height - node.left.height
+            node_children_right_diff = node.height - node.right.height
+            if node_children_left_diff == 1 and node_children_right_diff == 2:
+                new_subtree_root = AVLTree(parent).rotate_right()
+                if parent.parent.left.key == parent.key:
+                    parent.parent.left = new_subtree_root
+                else:
+                    parent.parent.right = new_subtree_root
+                parent.height -= 1
+            else:
+                new_subtree_root = AVLTree(parent).double_rotation()
+                new_subtree_root.height += 1
+                if parent.parent.left.key == parent.key:
+                    parent.parent.left = new_subtree_root
+                else:
+                    parent.parent.right = new_subtree_root
+                parent.height -= 1
+                node.height -= 1
+        return promotions
 
-	"""deletes node from the dictionary
+    def find_insertion_place(self, key):
+        node = self.root
+        is_right = False
+        path_len_counter = 0
+        while node.is_real_node():
+            if node.key > key:
+                node = node.left
+                is_right = False
+            else:
+                node = node.right
+                is_right = True
+            path_len_counter += 1
+        return node, is_right, path_len_counter
 
-	@type node: AVLNode
-	@pre: node is a real pointer to a node in self
-	"""
-	def delete(self, node):
-		return	
+    def insert(self, key, val):
+        insertion_place_details = self.find_insertion_place(key)
+        node = insertion_place_details[0]
+        is_right = insertion_place_details[1]
+        path_len_counter = insertion_place_details[2]
+        needs_rebalance = False
+        if node.parent.is_leaf():
+            needs_rebalance = True
+        new_node = AVLNode(key, val, 0)
+        new_node.left = AVLNode(None, None, parent=new_node)
+        new_node.right = AVLNode(None, None, parent=new_node)
+        new_node.parent = node.parent
+        if is_right:
+            node.parent.right = new_node
+        else:
+            node.parent.left = new_node
+        promotions = 0
+        if needs_rebalance:
+            new_node.parent.height += 1
+            if new_node.parent.key != self.root.key:
+                promotions = 1 + self.rebalance(new_node.parent)
+        return new_node, path_len_counter, promotions
 
-	
-	"""joins self with item and another AVLTree
+    """inserts a new node into the dictionary with corresponding key and value, starting at the max
 
-	@type tree2: AVLTree 
-	@param tree2: a dictionary to be joined with self
-	@type key: int 
-	@param key: the key separting self and tree2
-	@type val: string
-	@param val: the value corresponding to key
-	@pre: all keys in self are smaller than key and all keys in tree2 are larger than key,
-	or the opposite way
-	"""
-	def join(self, tree2, key, val):
-		return
+    @type key: int
+    @pre: key currently does not appear in the dictionary
+    @param key: key of item that is to be inserted to self
+    @type val: string
+    @param val: the value of the item
+    @rtype: (AVLNode,int,int)
+    @returns: a 3-tuple (x,e,h) where x is the new node,
+    e is the number of edges on the path between the starting node and new node before rebalancing,
+    and h is the number of PROMOTE cases during the AVL rebalancing
+    """
 
+    def finger_insert(self, key, val):
+        return None, -1, -1
 
-	"""splits the dictionary at a given node
+    """deletes node from the dictionary
 
-	@type node: AVLNode
-	@pre: node is in self
-	@param node: the node in the dictionary to be used for the split
-	@rtype: (AVLTree, AVLTree)
-	@returns: a tuple (left, right), where left is an AVLTree representing the keys in the 
-	dictionary smaller than node.key, and right is an AVLTree representing the keys in the 
-	dictionary larger than node.key.
-	"""
-	def split(self, node):
-		return None, None
+    @type node: AVLNode
+    @pre: node is a real pointer to a node in self
+    """
 
-	
-	"""returns an array representing dictionary 
+    def delete(self, node):
+        return
 
-	@rtype: list
-	@returns: a sorted list according to key of touples (key, value) representing the data structure
-	"""
-	def avl_to_array(self):
-		return None
+    """joins self with item and another AVLTree
 
+    @type tree2: AVLTree 
+    @param tree2: a dictionary to be joined with self
+    @type key: int 
+    @param key: the key separting self and tree2
+    @type val: string
+    @param val: the value corresponding to key
+    @pre: all keys in self are smaller than key and all keys in tree2 are larger than key,
+    or the opposite way
+    """
 
-	"""returns the node with the maximal key in the dictionary
+    def join(self, tree2, key, val):
+        return
 
-	@rtype: AVLNode
-	@returns: the maximal node, None if the dictionary is empty
-	"""
-	def max_node(self):
-		return None
+    """splits the dictionary at a given node
 
-	"""returns the number of items in dictionary 
+    @type node: AVLNode
+    @pre: node is in self
+    @param node: the node in the dictionary to be used for the split
+    @rtype: (AVLTree, AVLTree)
+    @returns: a tuple (left, right), where left is an AVLTree representing the keys in the 
+    dictionary smaller than node.key, and right is an AVLTree representing the keys in the 
+    dictionary larger than node.key.
+    """
 
-	@rtype: int
-	@returns: the number of items in dictionary 
-	"""
-	def size(self):
-		return -1	
+    def split(self, node):
+        return None, None
 
+    """returns an array representing dictionary 
 
-	"""returns the root of the tree representing the dictionary
+    @rtype: list
+    @returns: a sorted list according to key of touples (key, value) representing the data structure
+    """
 
-	@rtype: AVLNode
-	@returns: the root, None if the dictionary is empty
-	"""
-	def get_root(self):
-		return None
+    def avl_to_array(self):
+        if not self.root.is_real_node():
+            return []
+        root_tup = (self.root.key, self.root.value)
+        if self.root.is_leaf():
+            return [root_tup]
+        left_tree = AVLTree(self.root.left)
+        right_tree = AVLTree(self.root.right)
+        return left_tree.avl_to_array() + [root_tup] + right_tree.avl_to_array()
+
+    """returns the node with the maximal key in the dictionary
+
+    @rtype: AVLNode
+    @returns: the maximal node, None if the dictionary is empty
+    """
+
+    def max_node(self):
+        return None
+
+    """returns the number of items in dictionary 
+
+    @rtype: int
+    @returns: the number of items in dictionary 
+    """
+
+    def size(self):
+        if not self.root.is_real_node():
+            return 0
+        if self.root.is_leaf():
+            return 1
+        return AVLTree(self.root.left).size() + AVLTree(self.root.right).size() + 1
+
+    """returns the root of the tree representing the dictionary
+
+    @rtype: AVLNode
+    @returns: the root, None if the dictionary is empty
+    """
+
+    def get_root(self):
+        return None
---
+++ tests.py
@@ -0,0 +1,213 @@
+from AVLTree import AVLNode, AVLTree
+
+from pytest import fixture
+
+
+@fixture
+def medium_test_tree():
+    root = AVLNode(key=4, value="4")
+
+    # Level 1
+    left = AVLNode(key=2, value="2")
+    right = AVLNode(key=6, value="6")
+    root.left = left
+    root.right = right
+    left.parent = root
+    right.parent = root
+
+    # Level 2
+    left_left = AVLNode(key=1, value="1")
+    left_right = AVLNode(key=3, value="3")
+    right_left = AVLNode(key=5, value="5")
+    right_right = AVLNode(key=7, value="7")
+
+    left.left = left_left
+    left.right = left_right
+    left_left.parent = left
+    left_right.parent = left
+
+    right.left = right_left
+    right.right = right_right
+    right_left.parent = right
+    right_right.parent = right
+
+    # Level 3
+    left_left_left = AVLNode(key=0, value="0")
+    right_right_right = AVLNode(key=8, value="8")
+
+    left_left.left = left_left_left
+    left_left_left.parent = left_left
+
+    right_right.right = right_right_right
+    right_right_right.parent = right_right
+
+    # Adding one more node to make 10 nodes
+    top_right = AVLNode(key=9, value="9")
+    right_right_right.right = top_right
+    top_right.parent = right_right_right
+
+    return AVLTree(root)
+
+
+@fixture
+def big_test_tree():
+    root = AVLNode(key=10, value="10")
+
+    # Level 1
+    left = AVLNode(key=5, value="5")
+    right = AVLNode(key=15, value="15")
+    root.left = left
+    root.right = right
+    left.parent = root
+    right.parent = root
+
+    # Level 2
+    left_left = AVLNode(key=3, value="3")
+    left_right = AVLNode(key=7, value="7")
+    right_left = AVLNode(key=13, value="13")
+    right_right = AVLNode(key=18, value="18")
+
+    left.left = left_left
+    left.right = left_right
+    left_left.parent = left
+    left_right.parent = left
+
+    right.left = right_left
+    right.right = right_right
+    right_left.parent = right
+    right_right.parent = right
+
+    # Level 3
+    left_left_left = AVLNode(key=1, value="1")
+    left_left_right = AVLNode(key=4, value="4")
+    left_right_left = AVLNode(key=6, value="6")
+    left_right_right = AVLNode(key=8, value="8")
+
+    right_left_left = AVLNode(key=12, value="12")
+    right_left_right = AVLNode(key=14, value="14")
+    right_right_left = AVLNode(key=16, value="16")
+    right_right_right = AVLNode(key=19, value="19")
+
+    left_left.left = left_left_left
+    left_left.right = left_left_right
+    left_left_left.parent = left_left
+    left_left_right.parent = left_left
+
+    left_right.left = left_right_left
+    left_right.right = left_right_right
+    left_right_left.parent = left_right
+    left_right_right.parent = left_right
+
+    right_left.left = right_left_left
+    right_left.right = right_left_right
+    right_left_left.parent = right_left
+    right_left_right.parent = right_left
+
+    right_right.left = right_right_left
+    right_right.right = right_right_right
+    right_right_left.parent = right_right
+    right_right_right.parent = right_right
+
+    # Level 4
+    left_left_left.left = AVLNode(key=0, value="0")
+    left_left_left.right = AVLNode(key=2, value="2")
+    left_left_left.left.parent = left_left_left
+    left_left_left.right.parent = left_left_left
+
+    left_left_right.left = AVLNode(key=None, value="None")
+    left_left_right.right = AVLNode(key=None, value="None")
+    left_left_right.left.parent = left_left_right
+    left_left_right.right.parent = left_left_right
+
+    left_right_left.left = AVLNode(key=None, value="None")
+    left_right_left.right = AVLNode(key=None, value="None")
+    left_right_left.left.parent = left_right_left
+    left_right_left.right.parent = left_right_left
+
+    left_right_right.left = AVLNode(key=None, value="None")
+    left_right_right.right = AVLNode(key=9, value="9")
+    left_right_right.left.parent = left_right_right
+    left_right_right.right.parent = left_right_right
+
+    right_left_left.left = AVLNode(key=11, value="11")
+    right_left_left.right = AVLNode(key=None, value="None")
+    right_left_left.left.parent = right_left_left
+    right_left_left.right.parent = right_left_left
+
+    right_left_right.left = AVLNode(key=None, value="None")
+    right_left_right.right = AVLNode(key=None, value="None")
+    right_left_right.left.parent = right_left_right
+    right_left_right.right.parent = right_left_right
+
+    right_right_left.left = AVLNode(key=None, value="None")
+    right_right_left.right = AVLNode(key=17, value="17")
+    right_right_left.left.parent = right_right_left
+    right_right_left.right.parent = right_right_left
+
+    right_right_right.left = AVLNode(key=None, value="None")
+    right_right_right.right = AVLNode(key=None, value="None")
+    right_right_right.left.parent = right_right_right
+    right_right_right.right.parent = right_right_right
+
+    # Add children with None keys to 0, 2, 9, 11, and 17
+    zero_node = left_left_left.left
+    zero_node.left = AVLNode(key=None, value="None")
+    zero_node.right = AVLNode(key=None, value="None")
+    zero_node.left.parent = zero_node
+    zero_node.right.parent = zero_node
+
+    two_node = left_left_left.right
+    two_node.left = AVLNode(key=None, value="None")
+    two_node.right = AVLNode(key=None, value="None")
+    two_node.left.parent = two_node
+    two_node.right.parent = two_node
+
+    nine_node = left_right_right.right
+    nine_node.left = AVLNode(key=None, value="None")
+    nine_node.right = AVLNode(key=None, value="None")
+    nine_node.left.parent = nine_node
+    nine_node.right.parent = nine_node
+
+    eleven_node = right_left_left.left
+    eleven_node.left = AVLNode(key=None, value="None")
+    eleven_node.right = AVLNode(key=None, value="None")
+    eleven_node.left.parent = eleven_node
+    eleven_node.right.parent = eleven_node
+
+    seventeen_node = right_right_left.right
+    seventeen_node.left = AVLNode(key=None, value="None")
+    seventeen_node.right = AVLNode(key=None, value="None")
+    seventeen_node.left.parent = seventeen_node
+    seventeen_node.right.parent = seventeen_node
+    return AVLTree(root)
+
+
+@fixture()
+def build_tree():
+    lst = [i for i in range(20)]
+    root = AVLNode(key=lst[0], value=str(lst[0]))
+    root.left = AVLNode(key=None, value=None, parent=root)
+    root.right = AVLNode(key=None, value=None, parent=root)
+    tree = AVLTree(root)
+    for i in lst[1:]:
+        tree.insert(key=lst[i], val=str(lst[i]))
+    return tree
+
+def print_tree(node, level=0, prefix="Root: "):
+    """if node.is_real_node():
+    """
+    print(" " * (level * 4) + prefix + f"(Key: {node.key}, Value: {node.value}, Height: {node.height})")
+    if node.left or node.right:
+        print_tree(node.left, level + 1, prefix="L--- ")
+        print_tree(node.right, level + 1, prefix="R--- ")
+
+
+def test_avl_size(build_tree):
+    print_tree(build_tree.root)
+    assert build_tree.size() == 20
+
+
+def test_avl_to_array(big_test_tree):
+    # [0,1,2,3,4,5]
+
+    assert big_test_tree.avl_to_array() == [(i, str(i)) for i in range(20)]
